60de19d227f77016a4ec1748b1493ac721edb7d2
juanq <juancarloandrade31@gmail.com>
2025-03-27 16:05:02 -0300

    Implementação da camada de dominio

---
+++ src/main/java/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package repository;
+
+import me.dio.decola_tech_2025.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
+
